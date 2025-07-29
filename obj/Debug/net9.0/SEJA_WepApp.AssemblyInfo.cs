a52a8412eae5b5ae9a946e61f31552740d050c36
SilasDev <tiilas777@gmail.com>
2025-07-28 22:27:11 -0300

    add logo e mudannça da palheta de cores

--- obj/Debug/net9.0/SEJA_WepApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/SEJA_WepApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SEJA_WepApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2817e9cc634ed10098725c4c6def645a689dd296")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b35ce8b1c720859fa088d8c77b72d6aa9aca7f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SEJA_WepApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SEJA_WepApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
