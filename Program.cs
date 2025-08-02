c5b75ea89abf3bbe3fd17696de2950748286dee7
SilasDev <tiilas777@gmail.com>
2025-08-02 14:26:03 -0300

    design pra front page incrementado baseado em designs do pinterest

--- Program.cs
+++ Program.cs
@@ -1,23 +1,25 @@
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authentication.Facebook;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Autenticação externa (Google, Facebook, etc.)
-builder.Services.AddAuthentication()
-
-    .AddGoogle(options =>
-    {
-        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-    })
-    .AddFacebook(options =>
-    {
-        options.AppId = builder.Configuration["Authentication:Facebook:AppId"];
-        options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
-    });
+// Configuração de autenticação externa
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+})
+.AddFacebook(options =>
+{
+    options.AppId = builder.Configuration["Authentication:Facebook:AppId"];
+    options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
+});
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
@@ -28,19 +30,12 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Pipeline de requisição
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error"); 
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 app.UseCookiePolicy();
-app.UseAuthentication(); // <- Aqui
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
