5b42436c2270848ccc0fc4f7753ed81391988b60
SILAS-JOB <tiilas777@gmail.com>
2025-08-23 17:10:36 -0300

    web.js script de animação, não ta pegando ainda, vou investigar! Adicionei o texto que o Jorge passou finalmente. Implementar uma front page só pode ser uma boa solução pra deploy logo

--- wwwroot/js/web.js
+++ wwwroot/js/web.js
@@ -6,3 +6,18 @@ setTimeout(() => {
         setTimeout(() => msg.remove(), 1000);
     }
 }, 5000);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.2});
+
+    elements.forEach(el => observer.observe(el));
+});
